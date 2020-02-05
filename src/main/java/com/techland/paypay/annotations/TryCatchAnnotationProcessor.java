9f8bc288e12ae510cd0bd82e72ffc34c36e0befa
harveyimama <harveyimama@yahoo.com>
2020-02-05 15:49:26 +0100

    last annotation

--- src/main/java/com/techland/paypay/annotations/TryCatchAnnotationProcessor.java
+++ src/main/java/com/techland/paypay/annotations/TryCatchAnnotationProcessor.java
@@ -1,8 +1,9 @@
 package com.techland.paypay.annotations;
 
-import java.io.IOException;
-import java.io.Writer;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
@@ -10,55 +11,51 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic.Kind;
-import javax.tools.JavaFileObject;
+import javax.lang.model.type.ExecutableType;
 
-@SupportedAnnotationTypes("com.javacodegeeks.advanced.processor.Immutable")
+@SupportedAnnotationTypes("com.javacodegeeks.advanced.processor.TryCatch")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 
 public class TryCatchAnnotationProcessor extends AbstractProcessor {
 
 	@Override
 	public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
-		
-		for( final Element element: roundEnv.getElementsAnnotatedWith( TryCatch.class ) ) {
+
+		for (TypeElement annotation : annotations) {
+
+			Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
+
+			Map<Boolean, List<Element>> annotatedMethods = annotatedElements.stream().collect(
+
+					Collectors.partitioningBy(
+							element -> {return true;}
+									)
+			);
+
+			List<Element> methods = annotatedMethods.get(true);
 			
-			if( element instanceof TypeElement ) {
-				
-				final TypeElement typeElement = ( TypeElement )element;
-				
-				final String className = typeElement.getSimpleName() + "TryCatch";
-				final JavaFileObject fileObject = processingEnv.getFiler().createSourceFile(typeElement.getQualifiedName() + "." + className);
-				
-				
-				for( final Element eclosedElement: typeElement.getEnclosedElements() ) {
-					
-				ElementKind g = eclosedElement.getKind();
-				
-				if(g.equals(ElementKind.METHOD))
-				{
-					
-					try( Writer writter = fileObject.openWriter() ) {
-						writter.append( "package " + eclosedElement.getQualifiedName() + ";" );
-						writter.append( "\\n\\n");
-						writter.append( "public class " + className + " {" );
-						writter.append( "\\n");
-						writter.append( "}");
-						}
-						catch( final IOException ex ) {
-						processingEnv.getMessager().printMessage(Kind.ERROR, ex.getMessage());
-						}
-				}
-				
-			}
+
+			methods.stream().forEach(action->(
+					 (ExecutableType)action.asType().toString()
+					)
+			      
+			     
+			    		
+
+					);
+			
+			
+			String className = ((TypeElement) methods.get(0).getEnclosingElement()).getQualifiedName().toString();
+			
+			Map<String,String> methodMap = methods.stream().collect(
+					Collectors.toMap(method ->method.getSimpleName().toString(),
+							method -> ((ExecutableType)method.asType()).getParameterTypes().get(0).toString()));
+
 			
 		}
-		
-		
-	}
+
 		return true;
-}
-	
+	}
+
 }
